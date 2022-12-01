7067fc52e3fef7482b1ac3e348a959b393242147
subcero123 <61670433+subcero123@users.noreply.github.com>
2022-11-30 20:20:26 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("labelChoice1").addEventListener("click", function(){
-    document.getElementById("bikerGroupName").hidden = false;    
-});
-document.getElementById("labelChoice2").addEventListener("click", function(){
-    document.getElementById("bikerGroupName").hidden = true;    
-});
-
