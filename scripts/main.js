c3998b8cff106d3e739d75a633d600c535110c1b
subcero123 <61670433+subcero123@users.noreply.github.com>
2022-11-30 20:20:02 -0600

    Added script files

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+document.getElementById("labelChoice1").addEventListener("click", function(){
+    document.getElementById("bikerGroupName").hidden = false;    
+});
+document.getElementById("labelChoice2").addEventListener("click", function(){
+    document.getElementById("bikerGroupName").hidden = true;    
+});
+
