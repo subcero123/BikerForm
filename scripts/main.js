d59b934dc6486d330fa4c4ff4b8df6927043aabc
subcero123 <subcero139@hotmail.com>
2023-02-14 19:05:52 -0600

    Cambio en algunas funciones para el nuevo estilo

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
-document.getElementById("labelChoice1").addEventListener("click", function(){
-    document.getElementById("bikerGroupName").hidden = false;    
+document.getElementById("biker-yes").addEventListener("click", function(){
+    document.getElementById("bikerInput").hidden = false;    
 });
-document.getElementById("labelChoice2").addEventListener("click", function(){
-    document.getElementById("bikerGroupName").hidden = true;    
+document.getElementById("biker-no").addEventListener("click", function(){
+    document.getElementById("bikerInput").hidden = true;    
 });
+
 
--- scripts/saveData.js
+++ scripts/saveData.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+// Importación de funciones Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getDatabase, ref, set, get, child, onValue, update } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js'
 import { getStorage, ref as refStorage, uploadBytes } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js';
@@ -15,11 +15,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 
-// Initialize Realtime Database and get a reference to the service
+// Inicializacion de la base de datos Firebase, y obtener referencias
 var storage = getStorage();
 var database = getDatabase(app);
 
-// Get population value at startup
+//Obtener la población actual de la base de datos
 async function getPop(){
   var population = get(child(ref(database), `users/Pop`)).then((snapshot) => {
     return snapshot.val(); 
@@ -29,59 +29,74 @@ async function getPop(){
 };
 var population = await getPop();
 
-// Get population on data change (Another user updated the population count)
+//Obtener la población actual si es cambiada en tiempo real
 onValue(ref(database, 'users/Pop'), (snapshot) => {
   population = snapshot.val();
   console.log(population);
 });
 
-// Save message to firebase
-function saveMessage(name, age, country, gender, bikerGroupName, population, file){
+//Guardar el mensaje en Firebase
+function saveMessage(name, age, country, gender, bikerGroupName,  opinion, opinion2, email, population, file){
   population = population +1;
   update(ref(database, 'users/'), {
     Pop: population
   });
   update(ref(database, 'users/' + population), {
-    name: name,
-    age: age,
-    country: country,
-    gender: gender,
-    bGroup: bikerGroupName,
+    Nombre: name,
+    Edad: age,
+    Estado: country,
+    Genero: gender,
+    Significado_Memorial: opinion,
+    Prevencion: opinion2,
+    GrupoMotociclista: bikerGroupName,
   });
-  var storageRef = refStorage(storage, 'Imagenes/'+population);
+  if(file != null){
+    var storageRef = refStorage(storage, 'Imagenes/'+population);
   uploadBytes(storageRef, file).then((snapshot) => {
     console.log('Imágen subida');
   }); 
+  }
 }
 
 
 
 
-// Listen for form submit
+//Agrega la capacidad de saber cuando fue presionado el botón para terminar la encuesta
 document.getElementById('contactForm').addEventListener('submit', submitForm);
 
-// Submit form
+//Funcion para obtener, y posteriormente enviar los datos capturados
 function submitForm(e){
     e.preventDefault();
 
-    // Get values
+    //Obtención de valores
+    
     var name = getInputVal('name');
     var age = getInputVal('age');
     var country = getInputVal('country');
-    var file =document.getElementById('fileInput').files[0];
+    var opinion = getInputVal('opinion');
+    var opinion2 = getInputVal('opinion2');
+    var email = getInputVal('email');
+
+    if(document.getElementById('fileInput').files.lenght != 0){
+      var file =document.getElementById('fileInput').files[0];
+    }
+    else{
+      var file = null; 
+    }
+    
 
     if(document.getElementById('gender-male').checked == true){
-        var gender = "Male";
+        var gender = "Hombre";
     }
     else if (document.getElementById('gender-female').checked == true){
-        var gender = "Female";
+        var gender = "Mujer";
     }
     else{
       alert("Select a gender");
     }
     
     if(document.getElementById('biker-yes').checked == true){
-      var bikerGroupName = getInputVal(bikerInput);
+      var bikerGroupName = getInputVal("bikerInput");
     }
     else if (document.getElementById('biker-no').checked == true){
       var bikerGroupName = "No";
@@ -89,14 +104,13 @@ function submitForm(e){
     else{
       alert("Biker group not selected");
     }
-    // Save message
-    saveMessage(name,age, country, gender, bikerGroupName, population, file);
+    //Guardar el mensaje, y enviarlo
+    saveMessage(name,age, country, gender, bikerGroupName, opinion, opinion2, email, population, file);
 
-    // Clear form
+    //Limpiar el formulario 
     document.getElementById('contactForm').reset();
 }
-// Function to get  form values
+//Funcion para obtener los valores dado un ID
 function getInputVal(id){
     return document.getElementById(id).value;
 }
-
